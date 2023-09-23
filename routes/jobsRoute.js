4bd7b60b979daf49a925bcf6d833efde1131e370
applescan <feliu.ren@gmail.com>
2023-09-23 23:59:35 +1200

    fix adding job

--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,6 +1,30 @@
 var express = require('express');
 var jobsController = require('../controllers/jobsController')
 var router = express.Router();
+let multer = require('multer'),
+    uuidv4 = require('uuid/v4')
+
+const DIR = 'public';
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, DIR);
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        cb(null, uuidv4() + '-' + fileName)
+    }
+});
+var upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+        }
+    }
+});
 
 router.get(`/all`, function (req, res) {
     jobsController.fetchJobs(req, res)
@@ -30,7 +54,7 @@ router.post(`/update/:id`, function (req, res) {
     jobsController.updateJob(req, res)
 })
 
-router.post(`/add`, function (req, res) {
+router.post(`/add`, upload.none(), function (req, res) {
     jobsController.addJobs(req, res)
 })
 
